ccd043f5aea2339c1951ee1ac39696f984b76fa4
root <root@DESKTOP-D1R6CPH.localdomain>
2023-10-06 01:58:41 +0100

    comment the code

--- shell_exercices/2_strtok.c
+++ shell_exercices/2_strtok.c
@@ -3,28 +3,12 @@
 
 char *_strtok(char *str, const char *delim)
 {
-    /*
-        have a static variable that track the lasttoken
-        if the str != NULL (i didnt start reading the token)
-            lasttoken the str(this is on the begining)
-        else if lasttoken equel to NULL it mean im at the end
-            return null
-        need to get the start of the token ignoring the delimiters
-        if the start is the end of the string
-            lasttoken take null and return lasttoken
-        need to calculate end of token 
-            count the number of character till i found a delimeter
-        if last element is not the end of the string
-            lastelement  = '\0'
-            lasttoken = latelemnt +1
-        else 
-            lasttoken = NULL
-        return token start
-    */
     
+    /*declare static variable that will hold the token*/ 
     static char *lastToken = NULL;
     char *tokenStart = NULL, *tokenEnd = NULL;
-
+    /* if str != NULL this is when 
+    we first start to tokenize the string*/
     if (str != NULL)
     {
         lastToken = str;
@@ -33,20 +17,28 @@ char *_strtok(char *str, const char *delim)
     {
         return (NULL);
     }
-
+    /*search for the start of the token ignoring the delimeter
+    at the beggining*/
     tokenStart = lastToken + strspn(lastToken, delim);
+    /*if after some delimeter or not we find the end of the string
+    we dont have a next token we return null*/
     if (*tokenStart == '\0')
     {
         lastToken = NULL;
         return(NULL);
     }
+    /*we find the end of the string were we find a delimiter or not*/
     tokenEnd = tokenStart + strcspn(tokenStart, delim);
+    /*if we didnt yet reached the end of the string
+        we mark the end of end of the string by \0
+        and we mark the next token
+    */
     if (*tokenEnd != '\0')
     {
         *tokenEnd = '\0';
         lastToken = tokenEnd + 1;
     }
-    else
+    else /*we reached the end of the string we dont have a next token*/
     {
         lastToken = NULL;
     }
